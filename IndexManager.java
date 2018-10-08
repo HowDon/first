73540bd5a15669661cc6df3735b1f2369b2dadd3
Dohow <donghao0613@126.com>
2018-10-09 00:39:51 +0800

    第三次修改

--- IndexManager.java
+++ IndexManager.java
@@ -4,33 +4,23 @@ public class IndexManager {
 	
 	
 	private IndexWriter indexWriter;
+	
 	@Before
 	public void init() throws Exception {
 		indexWriter = new IndexWriter(FSDirectory.open(new File("D:/temp/index")),
 				new IndexWriterConfig(Version.LATEST, new IKAnalyzer()));
 	}
-
-	
-	
 	
 	@Test
-	
 	public void addDocument() throws Exception {
-		
-		
 		
 		// 1）使用IndexWriter打开索引库
 		// 2）创建一个Document对象
-		
-		
 		Document document = new Document();
-		
-		
 		// 3）向Document中添加field
 		Field name = new TextField("name", "新添加的文档新添加的文档新添加的文档新添加的文档新添加的文档新添加的文档新添加的文档新添加的文档新添加的文档新添加的文档新添加的文档新添加的文档aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa apache", Store.YES);
 		name.setBoost(10f);
 		document.add( name);
-		
 		document.add(new TextField("name1", "新添加的文档1", Store.YES));
 		document.add(new TextField("name2", "新添加的文档2", Store.YES));
 		// 4）把文档对象添加到索引库
